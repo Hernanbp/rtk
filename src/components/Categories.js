d12604819f6f557191fbce678fb4315927a5e6b8
Femar 01 <femar01@femar.local>
2022-04-12 16:41:41 -0300

    added images to categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -11,37 +11,71 @@ export const Categories = () => {
           Fast
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/FastFood_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
         <li>
           Healthy
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/Healthy_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
-        <li>Breakfast</li>
+        <li>
+          Breakfast
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/Breakfast_CuisineCarousel@2x.png"
+            alt=""
+          />
+        </li>
         <li>
           American
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/American_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
         <li>
           Asian
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/Asian_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
         <li>
           Italian
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/Italian_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
         <li>
           Mexican
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/Mexican_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
         <li>
           Chinese
           <br />
           Food
+          <img
+            src="https://tb-static.uber.com/prod/web-eats-v2/categories/icons/Chinese_CuisineCarousel@2x.png"
+            alt=""
+          />
         </li>
       </ul>
     </div>
